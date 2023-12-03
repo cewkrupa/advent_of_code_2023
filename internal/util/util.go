c4e085511813eb497285812b3fed13827201c637
Elliott Krupa <elliottkrupa@honeycomb.io>
2023-12-03 13:04:51 -0500

    Create util init function

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,7 +1,25 @@
 package util
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
-func Test() {
-	fmt.Println("Test")
+func Init(one func(i string), two func(i string)) {
+	var input string
+	var part int
+	flag.StringVar(&input, "input", "./test_input.txt", "name of input file")
+	flag.IntVar(&part, "part", 0, "part of day")
+	flag.Parse()
+
+	fmt.Printf("input file: %v\n", input)
+
+	switch part {
+	case 1:
+		one(input)
+	case 2:
+		two(input)
+	default:
+		one(input)
+	}
 }
