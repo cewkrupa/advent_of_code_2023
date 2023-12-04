0a28bd4df39a3b7025738d353ebd3a9366575838
Elliott Krupa <elliottkrupa@honeycomb.io>
2023-12-04 10:07:07 -0500

    Day 4 part 2

--- day4/main.go
+++ day4/main.go
@@ -15,8 +15,16 @@ func main() {
 }
 
 type Game struct {
-	Winning []int
-	Have    []int
+	ID        int
+	Winning   []int
+	Have      []int
+	Score     int
+	Matches   int
+	Instances int
+}
+
+func (g Game) String() string {
+	return fmt.Sprintf("Game %v - Instances: %v Matches: %v Score: %v\n", g.ID, g.Instances, g.Matches, g.Score)
 }
 
 func one(file *os.File) {
@@ -28,6 +36,7 @@ func one(file *os.File) {
 func parseGames(f *os.File) []Game {
 	scanner := bufio.NewScanner(f)
 	var games []Game
+	gameNumber := 1
 	for scanner.Scan() {
 		line := scanner.Text()
 		hands := strings.Split(line, ":")
@@ -59,39 +68,67 @@ func parseGames(f *os.File) []Game {
 			hInts = append(hInts, h)
 		}
 
-		game := Game{wInts, hInts}
+		game := Game{ID: gameNumber, Winning: wInts, Have: hInts, Instances: 1}
 		games = append(games, game)
+		gameNumber++
 
 	}
 
 	return games
 }
 
-func scoreGames(gs []Game) int {
+func scoreGames(gList []Game) int {
 	var finalScore int
 
-	for i, g := range gs {
-		var gameScore int
-		var timesMatched int
-		for _, num := range g.Have {
-			matches := slices.ContainsFunc(g.Winning, func(idx int) bool {
-				return idx == num
-			})
-			if matches {
-				if timesMatched < 1 {
-					gameScore = 1
-				} else {
-					gameScore = gameScore * 2
-				}
-				timesMatched++
-			}
-		}
-		fmt.Printf("game %v: %v\n", i, gameScore)
-		finalScore += gameScore
+	for i, g := range gList {
+		scoreGame(&g)
+		finalScore += g.Score
+		gList[i] = g
 	}
 	return finalScore
 }
 
-func two(file *os.File) {
+func scoreGame(g *Game) {
+	var gameScore int
+	var timesMatched int
+	for _, num := range g.Have {
+		matches := slices.ContainsFunc(g.Winning, func(idx int) bool {
+			return idx == num
+		})
+		if matches {
+			if timesMatched < 1 {
+				gameScore = 1
+			} else {
+				gameScore = gameScore * 2
+			}
+			timesMatched++
+		}
+	}
+	g.Matches = timesMatched
+	g.Score = gameScore
+}
 
+func two(file *os.File) {
+	games := parseGames(file)
+	scoreGames(games)
+	totalCards := processGames(games)
+	fmt.Printf("totalCards: %v\n", totalCards)
+}
+
+func processGames(gameList []Game) int {
+	tableLength := len(gameList)
+	var totalCards int
+
+	for i, g := range gameList {
+		totalCards += g.Instances
+		if g.Matches > 0 {
+			for j := 1; j <= g.Matches && j+i < tableLength; j++ {
+				nextIdx := i + j
+				next := &gameList[nextIdx]
+
+				next.Instances = next.Instances + g.Instances
+			}
+		}
+	}
+	return totalCards
 }
