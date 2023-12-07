8aac22962ad1433ef7a06005e6e80345c45683b0
Elliott Krupa <elliottkrupa@honeycomb.io>
2023-12-06 21:51:11 -0500

    Day 6 part 1

---
+++ day6/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"aoc-2023/internal/util"
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	util.Init(one, two)
+}
+
+type Race struct {
+	Duration   int
+	Record     int
+	WaysToBeat []int
+}
+
+func one(f *os.File) {
+	races := parseRaces(f)
+
+	// y = -1x^2 + duration*x
+	// y := (-1 * (x ^ 2)) + (race.Duration * x)
+	// https://amsi.org.au/ESA_Senior_Years/SeniorTopic2/2a/2a_2content_9.html
+	// the 'record' line is a straight line across distance (perpendicular to Y-axis).
+	// We're looking for the intersection of the record line
+	// and the quadratic formula for acceleration,
+	// and then finding all whole numbers between those two points on the x axis
+	// line:
+	// y = mx + d
+	// y = 0(x) + race.Record
+
+	product := 1
+	for _, race := range races {
+		// 1.
+		//   a. construct formula (coefficients) for race
+
+		// ax^2+bx+c=mx+d => ax^2+(b−m)x+(c−d)=0
+		a := float64(-1) // this is related to accel, somehow
+		b := float64(race.Duration)
+		c := float64(0)
+
+		m := float64(0)
+		d := float64(race.Record)
+
+		//   b. intersect with record line
+		// ax^2+(b−m)x+(c−d)=0
+		b2 := b - m
+		c2 := c - d
+
+		// 2. find discriminant for formula
+		// quadratic: x = (-b ±√(b^2 - 4ac)) / 2a
+		discriminant := (b2 * b2) - (4 * a * c2)
+		sqr := math.Sqrt(discriminant)
+		// 3. Solve formula for points
+
+		p1 := (-b2 + sqr) / 2 * a
+		p2 := (-b2 - sqr) / 2 * a
+
+		// 4. find whole numbers between points
+
+		// if the points are whole numbers, they're _at_ the record
+		// and if they're at the record, we didn't beat it 🥈
+		// so let's add a little padding in one direction or the other.
+		if math.Mod(p1, 1.0) == 0 {
+			p1 = p1 + 0.1
+		}
+		if math.Mod(p2, 1.0) == 0 {
+			p2 = p2 - 0.1
+		}
+
+		p1NextInt := int(math.Ceil(p1))
+		p2PrevInt := int(math.Floor(p2))
+
+		for i := p1NextInt; i <= p2PrevInt; i++ {
+			race.WaysToBeat = append(race.WaysToBeat, i)
+		}
+		product = product * len(race.WaysToBeat)
+	}
+	fmt.Printf("product: %v\n", product)
+}
+
+func two(f *os.File) {
+
+}
+
+func parseRaces(f *os.File) []Race {
+	scanner := bufio.NewScanner(f)
+
+	var races []Race
+	r := regexp.MustCompile(`(\d+)`)
+
+	scanner.Scan()
+	timeLine := scanner.Text()
+
+	timeStrings := r.FindAllString(timeLine, -1)
+	for _, t := range timeStrings {
+		d, err := strconv.Atoi(t)
+		if err != nil {
+			panic(err)
+		}
+		race := Race{
+			Duration: d,
+		}
+		races = append(races, race)
+	}
+
+	scanner.Scan()
+	recordLine := scanner.Text()
+
+	recordStrings := r.FindAllString(recordLine, -1)
+	for i, rec := range recordStrings {
+		d, err := strconv.Atoi(rec)
+		if err != nil {
+			panic(err)
+		}
+		races[i].Record = d
+	}
+
+	return races
+}
